3feb0f86663b3d03c32ea1305fee7b091cb4a674
LittleJon5 <littleriz@gmail.com>
2015-04-02 14:13:12 -0600

    Just a few minor changes

--- Fred Forecaster version 2/helper.R
+++ Fred Forecaster version 2/helper.R
@@ -4,51 +4,48 @@
 ###############################################################################
 require(magrittr)
 
-# Change
+# ---------------------------------- Change
 chg <- function(tsData) {
   tsData %>% 
     diff %>% 
     na.omit
 }
 
-# Year over year change
+# --------------------------------- Year over year change
 ch1 <- function(tsData, n_obs_per_year) {
   tsData %>% 
     diff(lag = n_obs_per_year) %>% 
     na.omit
 }
 
-# Percentage change
+# --------------------------------- Percentage change
 pch <- function(tsData) {
   (tsData/lag(tsData, 1)-1) * 100 %>% 
     na.omit
 }
 
-# Year over year percentage change
+# ---------------------------------- Year over year percentage change
 pc1 <- function(tsData, n_obs_per_year) {
   (tsData/lag(tsData, n_obs_per_year)-1) * 100 %>% 
     na.omit
 }
 
-# Compounded annual change
+# ---------------------------------- Compounded annual change
 pca <- function(tsData, n_obs_per_year) {
   na.omit(((tsData/lag(tsData, 1))^n_obs_per_year-1) * 100)
 }
 
-# Continously compounded percentage change
+# ----------------------------- Continously compounded percentage change
 cch <- function(tsData) {
   na.omit((log(tsData) - log(lag(tsData, 1))) * 100)
 }
 
-# Continuously compounded annual change
+# ----------------------------- Continuously compounded annual change
 cca <- function(tsData, n_obs_per_year) {
   na.omit((log(tsData) - log(lag(tsData, 1))) * n_obs_per_year * 100)
 }
  
-# final plot function
-##################
-
-
+# ----------------------------- Final plot function
 ggforecast <- function(past, future, span.val, input.date){
   
   load("data\\recessions.RData")
@@ -124,5 +121,5 @@ past.data <- function(ets.data){
   
   return(plot.data)
 }
-#####
+
 
