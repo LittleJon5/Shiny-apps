35dab0dd2669645e98611cf73ac913f15233e30c
LittleJon5 <littleriz@gmail.com>
2015-04-03 11:05:04 -0600

    This the is the version of the app that reads data in from excel.

---
+++ Fred Forecaster(File upload)/ui.R
@@ -0,0 +1,69 @@
+library(shiny)
+library(forecast)
+library(ggplot2)
+library(fImport)
+
+shinyUI(fluidPage(
+# ------------------------- This loads the css file for app style
+  includeCSS("styles.css"),
+  
+  titlePanel(h1("Macro Economic Indicators")),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      fileInput("data", "Upload File",
+                accept = c(
+                  'text/csv',
+                  'text/comma-separated-values',
+                  'text/tab-separated-values',
+                  'text/plain',
+                  '.csv',
+                  '.tsv'
+                )),
+      
+      selectInput("manipulate", label = h3("Manipulate Options"),
+                  choices = c("No Transfromation",
+                              "Change",
+                              "Change From a Year Ago",
+                              "Percent Change",
+                              "Percent Change from a Year Ago",
+                              "Compounded Annual Rate of Change",
+                              "Continuously Compounded Rate of Change",
+                              "Countinuously Compounded Annual Rate of Change",
+                              "Natural Log"),
+                  selected = "No Transformation"),
+      
+      dateInput("date", 
+                label = h3("Date Input"), 
+                value = "1959-01-01"),
+      
+      numericInput("horizon", 
+                   label = h3("Forecast Horizon"), 
+                   value = 12), 
+      
+      sliderInput("smooth",
+                  label = h3("Smoother"),
+                  min=.05,
+                  max=.5,
+                  value= .21,
+                  animate = TRUE),
+      
+      numericInput("scalefactor", 
+                   label = h3("Scaled by:"), 
+                   value = 1)
+                            
+      
+    ),
+    
+    
+    
+    mainPanel(
+        tabsetPanel(
+          tabPanel("Forecast Graph", plotOutput("plot")),
+          tabPanel("Model", h2(verbatimTextOutput("text"))),
+          tabPanel("Forecasts", h2(tableOutput("table")))
+        )
+      
+             )
+  )))
